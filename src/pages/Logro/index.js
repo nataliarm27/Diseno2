055b57076c96d01a5dd8c6462f68548ecc6d419f
nataliarm27 <natalia.ramirez99@gmail.com>
2021-09-24 21:46:16 -0500

    ....

--- src/pages/Logro/index.js
+++ src/pages/Logro/index.js
@@ -23,7 +23,7 @@ function Felicitacion() {
         <h1 className="absolute flex z-10 right-2 top-5 text-white text-justify font-body text-base w-1/2">
         Antes de empezar a trabajar en tu futuro debes organizar tu presente.
         </h1>
-        <Boton className="absolute flex z-10 top-33 right-36 w-56 h-16 justify-center pt-4 font-body text-yellow-600 bg-yellow-400 border-2 border-white rounded-2xl p-2 brightness-125 hover:text-white hover:bg-yellow-600 transition duration-150 ease-in-out" type="submit" titulo="Empezar" href="/ordenar"/>
+        <Boton className="absolute flex z-10 top-33 right-36 w-56 h-16 justify-center pt-4 font-body text-yellow-600 bg-yellow-400 border-2 border-white rounded-2xl p-2 brightness-125 hover:text-white hover:bg-yellow-600 transition duration-150 ease-in-out" type="submit" titulo="Siguiente" href="/ordenar"/>
         </div>
         </div>
      

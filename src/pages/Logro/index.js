634305293464979283f87f60946f2d50e4023015
nataliarm27 <natalia.ramirez99@gmail.com>
2021-09-28 10:17:13 -0500

    .2

--- src/pages/Logro/index.js
+++ src/pages/Logro/index.js
@@ -15,15 +15,15 @@ function Felicitacion() {
         className="absolute flex z-10 right-31 top-2"
         src="/assets/¡eSO ES!.png" alt="" />  
          <img 
-        className="absolute flex z-10 right-36 top-8"
+        className="absolute flex z-10 right-36 top-7"
         src="/assets/Frame-1(2).png" alt="" /> 
          <img 
-        className="absolute flex z-10 right-36 top-17 h-60 w-56"
+        className="absolute flex z-10 right-36 top-14 h-60 w-56"
         src="/assets/perro.png" alt="" />       
-        <h1 className="absolute flex z-10 right-2 top-5 text-white text-justify font-body text-base w-1/2">
-        Antes de empezar a trabajar en tu futuro debes organizar tu presente.
+        <h1 className="absolute flex z-10 right-20 top-31 text-white text-justify font-body text-base w-1/2">
+        Ya estamos preparados para iniciar nuestro día, pero no olvides organizar tu espacio y así podrás tener tu mente y tus ideas claras para afrontar la jornada.
         </h1>
-        <Boton className="absolute flex z-10 top-33 right-36 w-56 h-16 justify-center pt-4 font-body text-yellow-600 bg-yellow-400 border-2 border-white rounded-2xl p-2 brightness-125 hover:text-white hover:bg-yellow-600 transition duration-150 ease-in-out" type="submit" titulo="Siguiente" href="/ordenar"/>
+        <Boton className="absolute flex z-10 top-36 right-34 w-56 h-16 justify-center pt-4 font-body text-yellow-600 bg-yellow-400 border-2 border-white rounded-2xl p-2 brightness-125 hover:text-white hover:bg-yellow-600 transition duration-150 ease-in-out" type="submit" titulo="Siguiente" href="/ordenar"/>
         </div>
         </div>
      

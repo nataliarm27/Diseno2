9bbba0b333c0e6eca812af42f1310729803d56c1
nataliarm27 <natalia.ramirez99@gmail.com>
2021-09-28 15:34:49 -0500

    aaa

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import Historia from "./pages/Historia";
 import Ordenar from "./pages/Ordenar";
 import logro from "./pages/Logro";
+import logro2 from "./pages/Logro2";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" component={Historia}/>
         <Route path="/ordenar" component={Ordenar}/>
         <Route path="/logro" component={logro}/>
+        <Route path="/logro2" component={logro2}/>
         {/*<Route exact path="/" component={IniciarSesion}
         <Route path="/registrar" component={Registrar} />
   <Route  path="/principal" render={() => <Principal />} />*/}
--- src/pages/Logro2/index.js
+++ src/pages/Logro2/index.js
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from "react";
 import {useHistory,Link} from 'react-router-dom';
 import Boton from "../../Components/Button";
 
-function Felicitacion() {
+function Felicitacion2() {
     return(
         <div className="flex bg-black">
         <img
@@ -21,7 +21,7 @@ function Felicitacion() {
         className="absolute flex z-10 right-36 top-14 h-60 w-56"
         src="/assets/perro.png" alt="" />       
         <h1 className="absolute flex z-10 right-20 top-31 text-white text-center font-body text-base w-1/2">
-        Ya estamos preparados para iniciar nuestro día, pero no olvides organizar tu espacio y así podrás tener tu mente y tus ideas claras para afrontar la jornada.
+        Ahora el siguiente paso es planificar tus actividades diarias donde tendrás espacios de descansos en los que podrás divertirte.
         </h1>
         <Boton className="absolute flex z-10 top-36 right-34 w-56 h-16 justify-center pt-4 font-body text-yellow-600 bg-yellow-400 border-2 border-white rounded-2xl p-2 brightness-125 hover:text-white hover:bg-yellow-600 transition duration-150 ease-in-out" type="submit" titulo="Siguiente" href="/ordenar"/>
         </div>
@@ -30,4 +30,4 @@ function Felicitacion() {
         );
 }
 
-export default Felicitacion
+export default Felicitacion2
